54ecddb3049a0f889627897afec2cfd42ad3bd24
towersxu <987427795@qq.com>
2015-04-18 10:39:16 +0800

    增加requeryJS

---
+++ requireJS/README.md
@@ -0,0 +1,2 @@
+##requireJS
+1)requireJS的配置文件中，baseUrl的值是相对require.js的路径的，而不是配置文件的相对路径的。
---
+++ requireJS/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 15-4-18.
+ */
+require.config({
+    baseUrl: '../js',
+    paths:{
+        jquery:'jquery-2.1.1'
+    }
+});
+require(['jquery'],function($){
+    console.log($().jquery);
+});

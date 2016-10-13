7519e62b891b20431a2ce2adf6cd910b17d33c11
xutao <987427795@qq.com>
2016-10-13 20:29:54 +0800

    webGL编程指南

---
+++ webGL编程指南/chpter01/step.js
@@ -0,0 +1,24 @@
+var stepIndex = 0
+var stepInfo = [
+  {
+    name: '创建背景',
+    src: './step01.js'
+  },
+  {
+    name: '绘制一个点',
+    src: './step02.js'
+  }
+]
+function appendStep() {
+  var st = document.createElement('script')
+  st.src = stepInfo[stepIndex].src
+  var s = document.getElementsByTagName('body')[0]
+  s.appendChild(st)
+}
+function nextStep(){
+  document.getElementById('name').innerHTML = stepInfo[stepIndex].name
+  appendStep()
+  stepIndex ++;
+}
+
+document.getElementById('nextStep').addEventListener('click',nextStep)
